9d1602837623aeb12a94b4bdb99d15d7ea994bc1
camosqueraba <camosqueraba@gmail.com>
2016-11-23 15:54:49 -0500

    agregando clases

--- app/src/main/java/unimagdalena/edu/co/miproyectopoo/MainActivity.java
+++ app/src/main/java/unimagdalena/edu/co/miproyectopoo/MainActivity.java
@@ -1,10 +1,9 @@
 package unimagdalena.edu.co.miproyectopoo;
 
-import android.support.annotation.IdRes;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
@@ -47,52 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-                /*if (tabId == R.id.tab_buscar) {
-                    // The tab with id R.id.tab_favorites was selected,
-                    // change your content accordingly.
-                    BuscarFragment f = new BuscarFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.contentContainer,f).commit();
-                } else */
+
             }
         });
     }
 }
 
-/*
- mBottomBar = BottomBar.attach(this,savedInstanceState);
-        mBottomBar.setItemsFromMenu(R.menu.menu_main,new OnMenuTabClickListener() {
-            @Override
-            public void onMenuTabSelected(@IdRes int i) {
-                if(i==R.id.Bottombaritemone)
-                {
-                    PeopleFragment f = new PeopleFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-                }
-                else if(i==R.id.Bottombaritemtwo)
-                {
-                    Cardsfragment f =new Cardsfragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-                }
-                else if(i==R.id.Bottombaritemthree)
-                {
-                    PhoneFragment f =new PhoneFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-                }
-                else if(i==R.id.Bottombaritemfour)
-                {
-                    MailsFragment f =new MailsFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-                }
-                else if(i==R.id.Bottombaritemfive)
-                {
-                    FavoritesFragment f =new FavoritesFragment();
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frame,f).commit();
-                }
-            }
-
-            @Override
-            public void onMenuTabReSelected(@IdRes int i) {
-
-            }
-        });
- */
